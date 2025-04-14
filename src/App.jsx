364d91fbce0b185e042f9b5b407c7cc711709883
Loai-101 <q9g8moh@gmail.com>
2025-04-14 09:25:18 -0700

    App.jsx Fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import {Routes ,Route} from 'react-router'
+import {Routes ,Route, BrowserRouter} from 'react-router'
 import Login from './pages/Login'
-// import Homepage from './pages/Homepage'
+import LandingPage from './pages/Landing'
 import Signup from './pages/Signup'
 import Navbar from './components/Navbar'
 import ValidateIsLoggedIn from './validators/ValidateIsLoggedIn'
@@ -12,26 +12,45 @@ import DeleteProject from './pages/DeleteProject'
 import CreateTask from './pages/CreateTask'
 import EditTask from './pages/EditTask'
 import DeleteTask from './pages/DeleteTask'
+import { authContext } from './context/AuthContext'
+import { useContext, useEffect } from 'react'
+import ProjectList from './pages/ProjectList'
+import { useNavigate } from 'react-router'
 
 function App() {
+  const { user } = useContext(authContext);
+  const navigate = useNavigate();
 
+  // useEffect(() => {
+  //   if (!user) {
+  //     navigate('/signup');
+  //   }
+  // }, [user, navigate]);
 
   return (
     <>
       <Navbar/>
       <Routes>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/project/create" element={<CreateProject/>}/>
-        <Route path="/project/:projectId/edit" element={<EditProject/>}/>
-        <Route path="/project/:projectId/delete" element={<DeleteProject/>}/>
-        <Route path="/project/:projectId/tasks/create" element={<CreateTask/>}/>
-        <Route path="/project/:projectId/tasks/:taskId/edit" element={<EditTask/>}/>
-        <Route path="/project/:projectId/tasks/:taskId/delete" element={<DeleteTask/>}/>
+        {user ? (
+          <>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/projects" element={<ProjectList />} />
+            <Route path="/project/create" element={<CreateProject/>}/>
+            <Route path="/project/:projectId/edit" element={<EditProject/>}/>
+            <Route path="/project/:projectId/delete" element={<DeleteProject/>}/>
+            <Route path="/project/:projectId/tasks/create" element={<CreateTask/>}/>
+            <Route path="/project/:projectId/tasks/:taskId/edit" element={<EditTask/>}/>
+            <Route path="/project/:projectId/tasks/:taskId/delete" element={<DeleteTask/>}/>
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </>
+        )}
       </Routes>
     </>
-    
-  )
+  );
 }
 
 export default App
