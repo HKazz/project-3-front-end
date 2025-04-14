2feeeeecea1d6c1aa834242212bcbe147bbab07c
Murtadha-Alalawi <murtadha.alawi04@gmail.com>
2025-04-14 19:38:31 +0300

    added project list

--- src/pages/ProjectList.jsx
+++ src/pages/ProjectList.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function ProjectList() {
+  const [projects, setProjects]= useState([]);
+  const [error, setError]= useState('');
+
+  useEffect(()=>{
+    async function getProjects() {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/projects`,{
+          headers: {Authorization: `Bearer ${token}`},
+        })
+        setProjects(response.data)
+      } catch (error) {
+        setError("Failed to fetch projects. Please try again")
+        console.error(error)
+      }
+    }
+
+    getProjects();
+  })
   return (
     <div>
-      
+      <h1>Your Projects</h1>
+     <ul>
+      {projects.map((project)=>(
+        <li key={project._id}>
+          <h3>{project.name}</h3>
+          <p>{project.description}</p>
+        </li>
+      ))}
+     </ul>
     </div>
   )
 }
